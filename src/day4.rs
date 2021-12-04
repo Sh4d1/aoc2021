aeaf2360ce6bb71d8887112ae93e4e1042044fbb
Patrik Cyvoct <patrik@ptrk.io>
2021-12-04 10:33:42 +0100

    rewrite day4
    Signed-off-by: Patrik Cyvoct <patrik@ptrk.io>

--- src/day4.rs
+++ src/day4.rs
@@ -1,120 +1,78 @@
-use std::collections::HashSet;
+use ndarray::Array2;
 
-#[derive(Debug, Clone)]
-pub struct Board {
-    init: Vec<Vec<u64>>,
-    possibles: Vec<Vec<u64>>,
-}
-
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub struct Bingo {
     draw: Vec<u64>,
     boards: Vec<Board>,
 }
 
-#[aoc_generator(day4)]
-pub fn input_generator(input: &str) -> Bingo {
-    let mut lines = input.lines();
-    let mut boards: Vec<Board> = Vec::new();
-    let draw = lines.next().unwrap().split(",").map(|d| d.parse::<u64>().unwrap()).collect();
-    lines.next();
-    let mut line = lines.next();
-    let mut tmp = vec![];
-    loop {
-        if line.is_none() || line.unwrap().trim().is_empty() {
-            let mut b = Board{init: tmp, possibles: vec![]};
-            let mut columns: Vec<Vec<u64>> = vec![vec![]; b.init[0].len()];
-            for (i, d) in b.init.iter().enumerate() {
-                for (j, e) in d.iter().enumerate() {
-                    columns[j].push(*e); 
-                }
-                b.possibles.push(d.to_vec());
-            }
-            b.possibles.append(&mut columns);
+#[derive(Debug)]
+pub struct Board {
+    grid: Array2<(u64, bool)>,
+    done: bool,
+}
 
-            boards.push(b);
-            tmp = vec![];
-            if line.is_none() {
-                break;
-            }
-        } else {
-            let numbers = line.unwrap().split_whitespace().map(|d| d.parse::<u64>().unwrap()).collect(); 
-            tmp.push(numbers);
+impl Bingo {
+    pub fn new(input: &str) -> Self {
+        let mut lines = input.lines();
+        let draw: Vec<u64> = lines.next().unwrap().split(",").map(|d| d.parse().unwrap()).collect();
+        lines.next();
+
+        let lines: Vec<&str> = lines.collect::<Vec<&str>>().into_iter().filter(|l| !l.is_empty()).collect();
+        Bingo{
+            draw: draw,
+            boards: lines.chunks(5).map(|l| {
+                let rows: Vec<Vec<(u64, bool)>> = l.iter().map(|l| l.split_whitespace().map(|d| (d.parse().unwrap(), false)).collect()).collect();
+                Board{
+                    grid: Array2::from_shape_fn((rows[0].len(),rows.len()), |(j,i)| rows[j][i]),
+                    done: false,
+                }
+            }).collect(),
         }
-        line = lines.next();
     }
-    Bingo{
-        draw: draw,
-        boards: boards,
+
+    pub fn run1(&mut self) -> u64 {
+        self.draw.clone().iter().find_map(
+            |d| 
+            self.boards.iter_mut()
+                .find_map(|b| b.check(d))
+        ).unwrap()
+    }
+
+    pub fn run2(&mut self) -> u64 {
+        *self.draw.clone().iter().flat_map(
+            |d| 
+            self.boards.iter_mut()
+                .filter(|b| !b.done)
+                .filter_map(|b| b.check(d)).collect::<Vec<u64>>()
+        ).collect::<Vec<u64>>().last().unwrap()
+    }
+}
+
+impl Board {
+    pub fn check(&mut self, v: &u64) -> Option<u64> {
+        for (n, found) in self.grid.iter_mut() {
+            if n == v {
+                *found = true;
+            }
+        }
+        if self.grid.rows().into_iter().any(|row| row.iter().all(|(_, found)| *found)) 
+            || self.grid.columns().into_iter().any(|col| col.iter().all(|(_, found)| *found)) {
+                self.done = true;
+                return Some(v * self.grid.iter().filter(|(_, f)| !*f).map(|(n,_)| n).sum::<u64>());
+        }
+        None
     }
 }
 
 #[aoc(day4, part1)]
-pub fn part1(input: &Bingo) -> u64 {
-    let mut i = (*input).clone();
-    let mut win = 0;
-    let mut res = 0;
-    for n in input.draw.iter() {
-        for b in i.boards.iter_mut() {
-            for p in b.possibles.iter_mut() {
-                p.retain(|&x| x != *n);
-                if p.is_empty(){
-                    win = *n;
-                }
-            }
-            if win != 0 {
-                let mut hs = HashSet::new();
-                for p in b.possibles.iter() {
-                    for n in p.iter() {
-                        if !hs.contains(n) {
-                            res += n;
-                            hs.insert(n);
-                        }
-                    }
-                }
-                break;
-            }
-        }
-        if win != 0 {
-            break;
-        }
-    }
-    res*win
+pub fn part1(input: &str) -> u64 {
+    Bingo::new(input).run1()
 }
 
 #[aoc(day4, part2)]
-pub fn part2(input: &Bingo) -> u64 {
-    let mut i = (*input).clone();
-    let mut win = 0;
-    let mut res = 0;
-    let mut n_wins = HashSet::new();
-    for n in input.draw.iter() {
-        for (j, b) in i.boards.iter_mut().enumerate() {
-            for p in b.possibles.iter_mut() {
-                p.retain(|&x| x != *n);
-                if p.is_empty(){
-                    win = *n;
-                    n_wins.insert(j);
-                }
-            }
-            if win != 0 && n_wins.len() == input.boards.len() {
-                let mut hs = HashSet::new();
-                for p in b.possibles.iter() {
-                    for n in p.iter() {
-                        if !hs.contains(n) {
-                            res += n;
-                            hs.insert(n);
-                        }
-                    }
-                }
-                break;
-            }
-        }
-        if n_wins.len() == input.boards.len() {
-            break;
-        }
-    }
-    res*win
+pub fn part2(input: &str) -> u64 {
+    Bingo::new(input).run2()
 }
 
 #[cfg(test)]
@@ -124,7 +82,7 @@ mod tests {
     #[test]
     fn example1() {
         assert_eq!(
-            part1(&input_generator(
+            part1(
                 "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
 
 22 13 17 11  0
@@ -144,7 +102,7 @@ mod tests {
 18  8 23 26 20
 22 11 13  6  5
  2  0 12  3  7"
-            )),
+            ),
             4512
         )
     }
@@ -152,7 +110,7 @@ mod tests {
     #[test]
     fn example2() {
         assert_eq!(
-            part2(&input_generator(
+            part2(
                 "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
 
 22 13 17 11  0
@@ -173,7 +131,7 @@ mod tests {
 22 11 13  6  5
  2  0 12  3  7"
 
-            )),
+            ),
             1924
         )
     }
