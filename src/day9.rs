b197a11a2631704170ffed99b21e285f6b1559ff
Patrik Cyvoct <patrik@ptrk.io>
2021-12-09 08:07:22 +0100

    simplify day9
    Signed-off-by: Patrik Cyvoct <patrik@ptrk.io>

--- src/day9.rs
+++ src/day9.rs
@@ -1,91 +1,74 @@
 use std::collections::HashSet;
 use ndarray::Array2;
 
-#[aoc_generator(day9)]
-pub fn input_generator(input: &str) -> Array2<u32> {
-    let rows: Vec<Vec<u32>> = input.lines().map(|l| l.split_whitespace().collect::<String>().chars().map(|c| c.to_digit(10).unwrap()).collect()).collect();
-    Array2::from_shape_fn((rows[0].len(),rows.len()), |(j,i)| rows[i][j])
+pub struct Heightmap {
+    grid: Array2<u8>,
 }
 
-pub fn is_local_min(input: &Array2<u32>, x: usize, y: usize) -> bool {
-    let v = input.get((x,y)).unwrap();
-    let up = input.get((x, y+1)); 
-    let mut down = None;
-    if y as i32 - 1 >= 0 {
-        down = input.get((x, y-1)); 
-    }
-    let mut left = None;
-    if x as i32 - 1 >= 0 {
-        left = input.get((x-1, y)); 
-    }
-    let right = input.get((x+1, y)); 
-    if up.is_some() && v >= up.unwrap() {
-        return false;
-    }
-    if down.is_some() && v >= down.unwrap() {
-        return false;
-    }
-    if left.is_some() && v >= left.unwrap() {
-        return false;
-    }
-    if right.is_some() && v >= right.unwrap() {
-        return false;
+impl Heightmap {
+    fn is_local_min(&self, x: usize, y: usize) -> bool {
+        for xi in std::cmp::max(0, x as i32 - 1) as usize ..=std::cmp::min(self.grid.shape()[0] as i32 - 1, x as i32 + 1) as usize {
+            for yi in std::cmp::max(0, y as i32 - 1) as usize..=std::cmp::min(self.grid.shape()[1] as i32 - 1, y as i32 + 1) as usize {
+                if (xi == x || yi == y) && (xi != x || yi != y) {
+                    if self.grid.get((x, y)).unwrap() >= self.grid.get((xi, yi)).unwrap() {
+                        return false;
+                    }
+                }
+            }
+        }
+        true
     }
 
-    true
+    pub fn solve_1(&self) -> u32 {
+        self.grid.indexed_iter().fold(0u32, |acc, ((x, y), &v)| {
+            if self.is_local_min(x, y) {
+                acc + v as u32 + 1
+            } else {
+                acc
+            }
+        })
+    }
+
+    fn n_higher(&self, x: usize, y: usize, visited: &mut HashSet<(usize, usize)>) -> u32 {
+        let mut res = 1;
+        visited.insert((x, y));
+        for xi in std::cmp::max(0, x as i32 - 1) as usize..=std::cmp::min(self.grid.shape()[0] as i32 - 1, x as i32 + 1) as usize {
+            for yi in std::cmp::max(0, y as i32 - 1) as usize..=std::cmp::min(self.grid.shape()[1] as i32 - 1, y as i32 + 1) as usize {
+                if (xi == x || yi == y) && (xi != x || yi != y) && self.grid.get((xi, yi)).unwrap() != &9 && !visited.contains(&(xi, yi)) {
+                    res += self.n_higher(xi, yi, visited);
+                }
+            }
+        }
+        res
+    }
+
+    pub fn solve_2(&self) -> u32 {
+        let mut basins = vec![];
+        for ((x,y), _) in self.grid.indexed_iter() {
+            if self.is_local_min(x, y) {
+                basins.push(self.n_higher(x, y, &mut HashSet::new()));
+            }
+        }
+        basins.sort_unstable_by(|a, b| b.cmp(a));
+        basins.iter().take(3).fold(1, |acc, v| acc*v)
+    }
+
+}
+
+#[aoc_generator(day9)]
+pub fn input_generator(input: &str) -> Heightmap {
+    let rows: Vec<Vec<u8>> = input.lines().map(|l| l.split_whitespace().collect::<String>().chars().map(|c| c.to_digit(10).unwrap() as u8).collect()).collect();
+    Heightmap{grid: Array2::from_shape_fn((rows.len(),rows[0].len()), |(i,j)| rows[i][j])}
 }
 
 #[aoc(day9, part1)]
-pub fn part1(input: &Array2<u32>) -> u32 {
-    input.indexed_iter().fold(0u32, |acc, ((x,y), &v)| {
-        if is_local_min(input, x, y) {
-            acc + v + 1
-        } else {
-            acc
-        }
-    })
-}
-
-pub fn n_higher(input: &Array2<u32>, x: usize, y: usize, visited: &mut HashSet<(usize, usize)>) -> u32 {
-    let up = input.get((x, y+1)); 
-    let mut down = None;
-    if y as i32 - 1 >= 0 {
-        down = input.get((x, y-1)); 
-    }
-    let mut left = None;
-    if x as i32 - 1 >= 0 {
-        left = input.get((x-1, y)); 
-    }
-    let right = input.get((x+1, y)); 
-    visited.insert((x,y));
-
-    let mut res = 1;
-    if up.is_some() && up.unwrap() != &9 && !visited.contains(&(x, y+1)) {
-        res += n_higher(input, x, y+1, visited);
-    }
-    if down.is_some() && down.unwrap() != &9 && !visited.contains(&(x, y-1)) {
-        res += n_higher(input, x, y-1, visited);
-    }
-    if left.is_some() && left.unwrap() != &9 && !visited.contains(&(x-1, y)) {
-        res += n_higher(input, x-1, y, visited);
-    }
-    if right.is_some() && right.unwrap() != &9 && !visited.contains(&(x+1, y)) {
-        res += n_higher(input, x+1, y, visited);
-    }
-
-    res
+pub fn part1(input: &Heightmap) -> u32 {
+    input.solve_1()
 }
 
 #[aoc(day9, part2)]
-pub fn part2(input: &Array2<u32>) -> u32 {
-    let mut basins = vec![];
-    for ((x,y), &v) in input.indexed_iter() {
-        if is_local_min(input, x, y) {
-            basins.push(n_higher(input, x, y, &mut HashSet::new()));
-        }
-    }
-    basins.sort();
-    basins.iter().rev().take(3).fold(1, |acc, v| acc*v)
+pub fn part2(input: &Heightmap) -> u32 {
+    input.solve_2()
 }
 
 #[cfg(test)]
