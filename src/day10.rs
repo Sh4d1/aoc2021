e4c1442391dba00265a4fbb4bab787ed71743073
Patrik Cyvoct <patrik@ptrk.io>
2021-12-10 07:37:38 +0100

    rewrite day10
    Signed-off-by: Patrik Cyvoct <patrik@ptrk.io>

--- src/day10.rs
+++ src/day10.rs
@@ -1,86 +1,104 @@
+#[derive(PartialEq, Eq, Clone)]
+pub enum ChunkType {
+    P,
+    B,
+    C,
+    A,
+}
+use ChunkType::*;
+
+#[derive(Clone)]
+pub struct Chunk(ChunkType, bool, usize, usize);
+
+pub struct Line(Vec<Chunk>);
+
+impl Line {
+    fn get_corrupted_chunk(&self) -> Option<Chunk> {
+        let mut visited = vec![];
+        for c in self.0.iter() {
+            if c.1 {
+                visited.push(c);
+                continue;
+            }
+            if visited.pop().unwrap().0 != c.0 {
+                return Some((*c).clone());
+            }
+        }
+        None
+    }
+    fn get_end(&self) -> Option<Vec<Chunk>> {
+        if self.get_corrupted_chunk().is_some() {
+            return None;
+        }
+        Some(
+            self.0
+                .iter()
+                .fold(vec![], |mut acc, c| {
+                    if c.1 {
+                        acc.push(c.clone());
+                    } else {
+                        acc.pop();
+                    }
+                    acc
+                })
+                .into_iter()
+                .rev()
+                .collect(),
+        )
+    }
+}
+
+pub struct Navigation {
+    lines: Vec<Line>,
+}
+
 #[aoc_generator(day10)]
-pub fn input_generator(input: &str) -> Vec<Vec<char>> {
-    input.lines().map(|l| l.chars().collect()).collect()
+pub fn input_generator(input: &str) -> Navigation {
+    Navigation {
+        lines: input
+            .lines()
+            .map(|l| {
+                Line(
+                    l.chars()
+                        .map(|c| match c {
+                            c if c == '(' || c == ')' => Chunk(P, c == '(', 3, 1),
+                            c if c == '[' || c == ']' => Chunk(B, c == '[', 57, 2),
+                            c if c == '{' || c == '}' => Chunk(C, c == '{', 1197, 3),
+                            c if c == '<' || c == '>' => Chunk(A, c == '<', 25137, 4),
+                            _ => unreachable!(),
+                        })
+                        .collect(),
+                )
+            })
+            .collect(),
+    }
 }
 
 #[aoc(day10, part1)]
-pub fn part1(input: &[Vec<char>]) -> usize {
-    let mut res = 0;
-    for i in input {
-        let mut pushed = vec![];
-        let mut corrupted = None;
-        for j in i {
-            match j {
-                '{' => pushed.push(j),
-                '(' => pushed.push(j),
-                '[' => pushed.push(j),
-                '<' => pushed.push(j),
-                _ => {
-                    let mut last = pushed.pop();
-                    match last {
-                        Some(&c) if c == '{' && *j != '}' => corrupted = Some(*j),
-                        Some(&c) if c == '(' && *j != ')' => corrupted = Some(*j),
-                        Some(&c) if c == '[' && *j != ']' => corrupted = Some(*j),
-                        Some(&c) if c == '<' && *j != '>' => corrupted = Some(*j),
-                        _ => (),
-                    }
-                }
+pub fn part1(input: &Navigation) -> usize {
+    input
+        .lines
+        .iter()
+        .filter_map(|l| {
+            if let Some(c) = l.get_corrupted_chunk() {
+                Some(c.2)
+            } else {
+                None
             }
-            if corrupted.is_some() {
-                match corrupted.unwrap() {
-                    '}' => res += 1197,
-                    ']' => res += 57,
-                    ')' => res += 3,
-                    '>' => res += 25137,
-                    _ => unreachable!(),
-                }
-                break;
-            }
-        }
-    }
-    res
+        })
+        .sum()
 }
 
 #[aoc(day10, part2)]
-pub fn part2(input: &[Vec<char>]) -> usize {
-    let mut res = vec![];
-    for i in input {
-        let mut pushed = vec![];
-        let mut corrupted = None;
-        for j in i {
-            match j {
-                '{' => pushed.push(j),
-                '(' => pushed.push(j),
-                '[' => pushed.push(j),
-                '<' => pushed.push(j),
-                _ => {
-                    let mut last = pushed.pop();
-                    match last {
-                        Some(&c) if c == '{' && *j != '}' => corrupted = Some(*j),
-                        Some(&c) if c == '(' && *j != ')' => corrupted = Some(*j),
-                        Some(&c) if c == '[' && *j != ']' => corrupted = Some(*j),
-                        Some(&c) if c == '<' && *j != '>' => corrupted = Some(*j),
-                        _ => (),
-                    }
-                }
-            }
-            if corrupted.is_some() {
-                break;
-            }
-        }
-        if corrupted.is_none() {
-            res.push(pushed.iter().rev().fold(0, |mut acc, c| {
-                acc *= 5;
-                match c {
-                    '(' => acc + 1,
-                    '[' => acc + 2,
-                    '{' => acc + 3,
-                    '<' => acc + 4,
-                    _ => acc,
-                }
-            }));
-        }
-    }
+pub fn part2(input: &Navigation) -> usize {
+    let mut res = input
+        .lines
+        .iter()
+        .filter_map(|l| {
+            l.get_end()
+                .map(|cs| cs.iter().fold(0, |acc, c| acc * 5 + c.3))
+        })
+        .collect::<Vec<_>>();
     res.sort_unstable();
     res[res.len() / 2]
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@
 extern crate aoc_runner_derive;
 
 pub mod day1;
+pub mod day10;
 pub mod day2;
 pub mod day3;
 pub mod day4;
@@ -13,5 +14,4 @@ pub mod day6;
 pub mod day7;
 pub mod day8;
 pub mod day9;
-pub mod day10;
 aoc_lib! { year = 2021 }
